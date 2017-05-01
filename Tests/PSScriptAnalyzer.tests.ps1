ff4eb08bf7b8be42e844a6bb6b9568c9400c9fab
Jordan Hamilton <jordan-hamilton@users.noreply.github.com>
2017-04-30 21:12:42 -0600

    Updated script path in tests

--- Tests/PSScriptAnalyzer.tests.ps1
+++ Tests/PSScriptAnalyzer.tests.ps1
@@ -1,6 +1,6 @@
 Describe 'Testing against PSScriptAnalyzer rules' {
 	Context 'PSScriptAnalyzer Standard Rules' {
-		$analysis = Invoke-ScriptAnalyzer -Path '..\Deploy-Application.ps1'
+		$analysis = Invoke-ScriptAnalyzer -Path 'Deploy-Application.ps1'
 		$scriptAnalyzerRules = Get-ScriptAnalyzerRule
 		forEach ($rule in $scriptAnalyzerRules) {
 			It "Should pass $rule" {
